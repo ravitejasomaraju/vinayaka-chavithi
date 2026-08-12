150aa363eff16a798fd2e3610e9b9c0fcaf5ac76
Raviteja Somaraju <ravitejasomaraju5@gmail.com>
2026-08-12 17:14:41 +0530

    Prepare database and secret key for deployment

--- database.py
+++ database.py
@@ -6,6 +6,8 @@ def get_connection():
     database_url = os.environ.get("DATABASE_URL")
 
     if not database_url:
-        raise RuntimeError("DATABASE_URL environment variable is not set")
+        raise RuntimeError(
+            "DATABASE_URL environment variable is not set"
+        )
 
     return psycopg.connect(database_url)
